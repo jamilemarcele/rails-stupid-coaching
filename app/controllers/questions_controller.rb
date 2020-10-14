593a9a5f6b84a5d3e8414b08aa4dc9a8279d7fd2
Jamile Radloff <jamilemarcele@gmail.com>
2020-10-14 08:57:21 +0200

    coach questions exercise done

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -7,7 +7,7 @@ class QuestionsController < ApplicationController
     @question = params[:question]
     if @question == "I am going to work"
       @answer = "Great"
-    elsif @question[-1s] #@question.include?("?")
+    elsif @question[-1s] # this one also works => @question.include?("?") However, this one will answer even if I have a ? in the beginning
       @answer = "Silly question, get dressed and go to work!"
     else
       @answer = "I don't care, get dressed and go to work!"
